a7a14417e8b83068f2f4357c31117dc969c83d94
JWhof <romanczyy@gmail.com>
2025-01-09 16:57:19 +0100

    add robot moving

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -178,6 +178,37 @@
 	function deletePath(pathId) {
 		paths.update(paths => paths.filter(path => path.id !== pathId));
 	}
+
+	let scrubValue = 0;
+	let robotX = 12;
+	let robotY = 96;
+
+	function updateRobotPosition() {
+		let totalPoints = 0;
+		$paths.forEach(path => {
+			totalPoints += path.bezierCurvePoints.length;
+		});
+
+		let accumulatedPoints = 0;
+		for (let path of $paths) {
+			if (scrubValue <= (accumulatedPoints + path.bezierCurvePoints.length) / totalPoints * 100) {
+				const relativeScrubValue = (scrubValue - accumulatedPoints / totalPoints * 100) / (path.bezierCurvePoints.length / totalPoints * 100);
+				const pointIndex = Math.floor(relativeScrubValue * (path.bezierCurvePoints.length - 1));
+				const point = path.bezierCurvePoints[pointIndex];
+				if (point) {
+					robotX = point.x;
+					robotY = point.y;
+				}
+				break;
+			}
+			accumulatedPoints += path.bezierCurvePoints.length;
+		}
+	}
+
+
+
+
+
 </script>
   
 <style>
@@ -235,7 +266,7 @@
 		height: 2%;
 		border-radius: 50%;
 		transform: translate(-50%, 50%);
-		z-index: 1;
+		z-index: 2;
 	}
 
 	.point:hover {
@@ -247,18 +278,21 @@
 	.curve {
 		stroke-width: 0.7;
 		fill: none;
+		position: absolute;
+		z-index: 3;
 	}
 
-	.menu {
+	.robot-options-menu {
 		display: flex;
 		justify-content: space-between;
 		align-items: left;
 		padding: 1rem;
 		flex-grow: 1;
+		height: 45vh;
 	}
 
 
-	.menu > * {
+	.robot-options-menu > * {
 		margin: 0.5rem;
 	}
 
@@ -329,7 +363,7 @@
 		width: 125px;
 	}
 
-	.path-header > button {
+	/* .path-header > button {
 		background: #B1F0F7;
 		border: none;
 		padding: 0.5rem 1.5rem;
@@ -337,7 +371,7 @@
 		border-radius: 5px;
 		cursor: pointer;
 		font-size: 0.75rem;
-	}
+	} */
 
 	.color-circle {
 		-webkit-appearance: none;
@@ -392,27 +426,85 @@
 		font-size: 0.75rem;
 	}
 
+	#robot {
+		position: absolute;
+		transform: translate(-50%, 50%);
+		z-index: 1;
+	}
+
+	.scrubbing-bar {
+		display: flex;
+		align-items: center;
+		background: white;
+		padding: 0.5rem;
+		border-radius: 5px;
+		border: 1px solid #ccc;
+		margin: 0.5rem;
+	}
+
+	.scrubbing-bar label {
+		margin-right: 0.5rem;
+		font-size: 0.75rem;
+	}
+
+	.scrubbing-bar input[type="range"] {
+		-webkit-appearance: none;
+		width: 90vh;
+		height: 5px;
+		background: white;
+		border-radius: 0%;
+		outline: none;
+	}
+
+	.scrubbing-bar input[type="range"]::-webkit-slider-thumb {
+		-webkit-appearance: none;
+		width: 14px;
+		height: 14px;
+		background: #8EDCE6;
+		border-radius: 50%;
+		cursor: pointer;
+	}
+
+	.scrubbing-bar input[type="range"]::-moz-range-thumb {
+		width: 14px;
+		height: 14px;
+		background: white;
+		border-radius: 50%;
+		cursor: pointer;
+	}
 
 </style>
 
 <div>
 	<div class="header">
 		<h1 class="page-title">JPather</h1>
+
+		<div class="scrubbing-bar">
+			<label for="scrub">Scrub:</label>
+			<input type="range" id="scrub" min="0" max="100" step="1" bind:value={scrubValue} on:input={updateRobotPosition} />
+		</div>
+
 		<button on:click={exportControlPoints}>Export Control Points</button>
 	</div>
 	
 	<div class="container">
-		<div class="menu">
-			<div>
-				<h2 class="section-title">Robot Options</h2>
-				<div class="robot-options">
-					<label for="robot-length">Robot Length:</label>
-					<input id="robot-length" class="standard-input-box" type="number" step="0.01" bind:value={robotLength} />
+		<div class="settings-column">
+			<div class="robot-options-menu">
+				<div>
+					<h2 class="section-title">Robot Options</h2>
+					<div class="robot-options">
+						<label for="robot-length">Robot Length:</label>
+						<input id="robot-length" class="standard-input-box" type="number" step="0.01" bind:value={robotLength} />
+					</div>
+					<div class="robot-options">
+						<label for="robot-width">Robot Width:</label>
+						<input id="robot-width" class="standard-input-box" type="number" step="0.01" bind:value={robotWidth} />
+					</div>
 				</div>
-				<div class="robot-options">
-					<label for="robot-width">Robot Width:</label>
-					<input id="robot-width" class="standard-input-box" type="number" step="0.01" bind:value={robotWidth} />
-				</div>
+			</div>
+
+			<div class="field-settings">
+				
 			</div>
 		</div>
 
@@ -424,6 +516,7 @@
 				</div>
 				{/each}
 			{/each}
+			
 			<svg viewBox="0 0 144 144" width="100%" height="100%" style="position: absolute; top: 0; left: 0;">
 				{#each $paths as path}
 					<polyline
@@ -433,6 +526,15 @@
 					/>
 				{/each}
 			</svg>
+
+
+
+			{#if $paths.length > 0}
+				<img src="/drivetrain.png" alt="Robot" id="robot" style="width: {robotWidth / 144 * 100}%; height: {robotLength / 144 * 100}%; left: {(robotX / 144) * 100}%; bottom: {(robotY / 144) * 100}%;" />
+			{/if}
+
+
+
 		</div>
 		
 		<div class="paths">
@@ -445,6 +547,7 @@
 							<p class="path-title">Path {path.id + 1}</p>
 						</div>
 						<div class="add-and-remove">
+							<!-- svelte-ignore a11y-click-events-have-key-events -->
 							<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FF474D" on:click={() => deletePath(path.id)} style="cursor: pointer;"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
 							<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#90EE90" on:click={() => addControlPointToPath(path.id)} on:keydown={(e) => { if (e.key === 'Enter') addControlPointToPath(path.id, x, y); }} style="cursor: pointer;"><path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
 						</div>
