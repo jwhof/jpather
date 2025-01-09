ce1a7ab140bc2812e05906bc2a16f44137389450
JWhof <romanczyy@gmail.com>
2025-01-09 23:38:52 +0100

    add playback button, various bug fixes

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# JPather
+## JPather
 
-TODO:
-[ ] add greyed out trash can so user cant delete only path
-[ ] fix bugged z-index of robot over curve
-[ ] add path organization dragging functionality
-[ ] less border radii so its less kid friendly
-[ ] increase space b/t trash and add buttons
-[ ] fix scrub bar 0% and 100%'s are offset
-[ ] add playback button
+# TODO:
+- [ ] add greyed out trash can so user cant delete only path
+- [ ] fix bugged z-index of robot over curve
+- [ ] add path organization dragging functionality
+- [ ] less border radii so its less kid friendly
+- [ ] increase space b/t trash and add buttons
+- [x] fix scrub bar 0% and 100%'s are offset
+- [x] add playback button
+- [ ] fix path deletion indexing problem
+- [ ] add autoLink paths together
+- [ ] add labels for X and Y on the control point menu + bold the numbers?
+- [ ] add units for robot length and width
+- [ ] add dark mode
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,7 +2,6 @@
 	import { writable } from 'svelte/store';
   
 	let controlPoints = writable([]);
-	let bezierCurvePoints = writable([]);
 	let paths = writable([]);
 
 	let x = 0;
@@ -60,59 +59,6 @@
 		});
 	}
 
-	document.addEventListener('DOMContentLoaded', () => {
-		addPath();
-		generateBezierCurve($paths.length - 1);
-	});
-
-	document.addEventListener('mousedown', (event) => {
-		const field = document.querySelector('.field');
-		const rect = field.getBoundingClientRect();
-		const mouseX = event.clientX - rect.left;
-		const mouseY = event.clientY - rect.top;
-
-		let selectedPathId = null;
-		let selectedPointIndex = null;
-
-		$paths.forEach((path) => {
-			path.controlPoints.forEach((point, index) => {
-				const pointX = point.x / 144 * rect.width;
-				const pointY = rect.height - (point.y / 144 * rect.height);
-				const distance = Math.sqrt((mouseX - pointX) ** 2 + (mouseY - pointY) ** 2);
-				if (distance < 10) {
-					selectedPathId = path.id;
-					selectedPointIndex = index;
-				}
-			});
-		});
-
-		if (selectedPathId !== null && selectedPointIndex !== null) {
-			const movePoint = (moveEvent) => {
-				const newMouseX = moveEvent.clientX - rect.left;
-				const newMouseY = moveEvent.clientY - rect.top;
-				const newX = newMouseX / rect.width * 144;
-				const newY = 144 - (newMouseY / rect.height * 144);
-
-				paths.update(paths => {
-					const path = paths.find(p => p.id === selectedPathId);
-					if (path) {
-						path.controlPoints[selectedPointIndex] = { x: newX, y: newY };
-						path.bezierCurvePoints = calculateBezier(path.controlPoints, 100);
-					}
-					return paths;
-				});
-			};
-
-			const stopMove = () => {
-				document.removeEventListener('mousemove', movePoint);
-				document.removeEventListener('mouseup', stopMove);
-			};
-
-			document.addEventListener('mousemove', movePoint);
-			document.addEventListener('mouseup', stopMove);
-		}
-	});
-
 	function addPath() {
 		paths.update(paths => {
 			const newPath = {
@@ -150,16 +96,6 @@
 				y = 72 + Math.sin(angle) * distance;
 				path.controlPoints.splice(path.controlPoints.length - 1, 0, { x, y });
 				path.bezierCurvePoints = calculateBezier(path.controlPoints, 100);
-			}
-			return paths;
-		});
-	}
-
-	function toggleColorPicker(pathId) {
-		paths.update(paths => {
-			const path = paths.find(p => p.id === pathId);
-			if (path) {
-				path.showColorPicker = !path.showColorPicker;
 			}
 			return paths;
 		});
@@ -206,6 +142,113 @@
 	}
 
 
+	let isPlaying = false;
+	let wasPaused = false;
+	let isStartingFromBeginning = true;
+	$: animTime = 1.56 * $paths.length;
+	let intervalId = null;
+	let animInterval = 3;
+	let progress = 0;
+	let elapsedTime = 0;
+	let path = null;
+	let pathAnimTime = 0;
+
+	function playPath() {
+		if (isPlaying) return;
+		isPlaying = true;
+	
+		let currentPathIndex = isStartingFromBeginning ? 0 : Math.floor(scrubValue / 100 * $paths.length);
+		let pathStartTime = Date.now() - (isStartingFromBeginning ? 0 : (scrubValue % (100 / $paths.length)) / 100 * animTime * 1000);
+
+		intervalId = setInterval(() => {
+			elapsedTime = (Date.now() - pathStartTime) / 1000;
+			path = $paths[currentPathIndex];
+			pathAnimTime = animTime / $paths.length;
+			progress = elapsedTime / pathAnimTime;
+
+			if (progress < 0.5) {
+				progress = 2 * progress * progress;
+			} else {
+				progress = -1 + (4 - 2 * progress) * progress;
+			}
+
+			scrubValue = ((currentPathIndex + progress) / $paths.length) * 100;
+			updateRobotPosition();
+
+			if (elapsedTime >= pathAnimTime) {
+				currentPathIndex = (currentPathIndex + 1) % $paths.length;
+				pathStartTime = Date.now();
+			}
+		}, animInterval);
+	}
+
+	function pausePath() {
+		isPlaying = false;
+		if (intervalId) {
+			clearInterval(intervalId);
+			intervalId = null;
+		}
+		wasPaused = true;
+		isStartingFromBeginning = false;
+	}
+
+
+
+	document.addEventListener('DOMContentLoaded', () => {
+		addPath();
+		generateBezierCurve($paths.length - 1);
+	});
+
+	document.addEventListener('mousedown', (event) => {
+		const field = document.querySelector('.field');
+		const rect = field.getBoundingClientRect();
+		const mouseX = event.clientX - rect.left;
+		const mouseY = event.clientY - rect.top;
+
+		let selectedPathId = null;
+		let selectedPointIndex = null;
+
+		$paths.forEach((path) => {
+			path.controlPoints.forEach((point, index) => {
+				const pointX = point.x / 144 * rect.width;
+				const pointY = rect.height - (point.y / 144 * rect.height);
+				const distance = Math.sqrt((mouseX - pointX) ** 2 + (mouseY - pointY) ** 2);
+				if (distance < 10) {
+					selectedPathId = path.id;
+					selectedPointIndex = index;
+				}
+			});
+		});
+
+		if (selectedPathId !== null && selectedPointIndex !== null) {
+			const movePoint = (moveEvent) => {
+				const newMouseX = moveEvent.clientX - rect.left;
+				const newMouseY = moveEvent.clientY - rect.top;
+				const newX = newMouseX / rect.width * 144;
+				const newY = 144 - (newMouseY / rect.height * 144);
+
+				paths.update(paths => {
+					const path = paths.find(p => p.id === selectedPathId);
+					if (path) {
+						path.controlPoints[selectedPointIndex] = { x: newX, y: newY };
+						path.bezierCurvePoints = calculateBezier(path.controlPoints, 100);
+					}
+					return paths;
+				});
+
+				updateRobotPosition();
+			};
+
+			const stopMove = () => {
+				document.removeEventListener('mousemove', movePoint);
+				document.removeEventListener('mouseup', stopMove);
+			};
+
+			document.addEventListener('mousemove', movePoint);
+			document.addEventListener('mouseup', stopMove);
+		}
+	});
+
 
 
 
@@ -249,13 +292,13 @@
 
 	.field {
 		position: relative;
-		width: 90vh;
-		height: 90vh;
-		max-width: 90vw;
-		max-height: 90vw;
+		width: 85vh;
+		height: 85vh;
+		max-width: 85vh;
+		max-height: 85vh;
 		min-width: 500px;
 		min-height: 500px;
-		background: url('/field-image.jpg') no-repeat center center;
+		background: url('/good-field-image.png') no-repeat center center;
 		background-size: cover;
 		border: 1px solid #ccc;
 	}
@@ -309,7 +352,7 @@
 		align-items: flex-start;
 		padding: 1rem;
 		flex-grow: 1;
-		max-height: 90vh;
+		max-height: 85vh;
 		overflow-y: auto;
 	}
 
@@ -438,40 +481,59 @@
 		background: white;
 		padding: 0.5rem;
 		border-radius: 5px;
-		border: 1px solid #ccc;
-		margin: 0.5rem;
-	}
 
-	.scrubbing-bar label {
-		margin-right: 0.5rem;
-		font-size: 0.75rem;
+		margin: 0.3rem;
+		box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.3);
+		border-radius: 20px;
+		
 	}
 
 	.scrubbing-bar input[type="range"] {
+		margin: 0.3rem;		
 		-webkit-appearance: none;
-		width: 90vh;
-		height: 5px;
-		background: white;
-		border-radius: 0%;
+		appearance: none;
+		padding: 0rem;
+		background: rgb(239, 238, 238);
+		border-radius: 12.5px;
 		outline: none;
+		width: 100%;
+		border: transparent;
+		align-items: center;
 	}
 
 	.scrubbing-bar input[type="range"]::-webkit-slider-thumb {
 		-webkit-appearance: none;
-		width: 14px;
-		height: 14px;
-		background: #8EDCE6;
+		appearance: none;
+		width: 20px;
+		height: 20px;
+		background: #B1F0F7;
 		border-radius: 50%;
 		cursor: pointer;
 	}
 
 	.scrubbing-bar input[type="range"]::-moz-range-thumb {
-		width: 14px;
-		height: 14px;
-		background: white;
+		width: 20px;
+		height: 20px;
+		background: #B1F0F7;
 		border-radius: 50%;
 		cursor: pointer;
 	}
+
+	.scrubbing-bar input[type="range"]::-ms-thumb {
+		width: 20px;
+		height: 20px;
+		background: #B1F0F7;
+		border-radius: 50%;
+		cursor: pointer;
+	}
+
+	.play-button {
+		cursor: pointer;
+		display: flex;
+		align-items: center;
+	}
+
+
 
 </style>
 
@@ -479,93 +541,106 @@
 	<div class="header">
 		<h1 class="page-title">JPather</h1>
 
-		<div class="scrubbing-bar">
-			<label for="scrub">Scrub:</label>
-			<input type="range" id="scrub" min="0" max="100" step="1" bind:value={scrubValue} on:input={updateRobotPosition} />
-		</div>
-
 		<button on:click={exportControlPoints}>Export Control Points</button>
 	</div>
+
+	<div class="main-content">
 	
-	<div class="container">
-		<div class="settings-column">
-			<div class="robot-options-menu">
-				<div>
-					<h2 class="section-title">Robot Options</h2>
-					<div class="robot-options">
-						<label for="robot-length">Robot Length:</label>
-						<input id="robot-length" class="standard-input-box" type="number" step="0.01" bind:value={robotLength} />
+		<div class="container">
+			<div class="settings-column">
+				<div class="robot-options-menu">
+					<div>
+						<h2 class="section-title">Robot Options</h2>
+						<div class="robot-options">
+							<label for="robot-length">Robot Length:</label>
+							<input id="robot-length" class="standard-input-box" type="number" step="0.01" bind:value={robotLength} />
+						</div>
+						<div class="robot-options">
+							<label for="robot-width">Robot Width:</label>
+							<input id="robot-width" class="standard-input-box" type="number" step="0.01" bind:value={robotWidth} />
+						</div>
 					</div>
-					<div class="robot-options">
-						<label for="robot-width">Robot Width:</label>
-						<input id="robot-width" class="standard-input-box" type="number" step="0.01" bind:value={robotWidth} />
-					</div>
+				</div>
+
+				<div class="field-settings">
+					
 				</div>
 			</div>
 
-			<div class="field-settings">
-				
-			</div>
-		</div>
-
-		<div class="field">
-			{#each $paths as path}
-				{#each path.controlPoints as { x, y }}
-				<div class="hover-point">
-					<div class="point" style="left: {x / 144 * 100}%; bottom: {y / 144 * 100}%; background: {path.color};"></div>
-				</div>
-				{/each}
-			{/each}
-			
-			<svg viewBox="0 0 144 144" width="100%" height="100%" style="position: absolute; top: 0; left: 0;">
+			<div class="field">
 				{#each $paths as path}
-					<polyline
-						class="curve"
-						points="{path.bezierCurvePoints.map(point => `${point.x},${144 - (point.y)}`).join(' ')}"
-						style="stroke: {path.color};"
-					/>
+					{#each path.controlPoints as { x, y }}
+					<div class="hover-point">
+						<div class="point" style="left: {x / 144 * 100}%; bottom: {y / 144 * 100}%; background: {path.color};"></div>
+					</div>
+					{/each}
 				{/each}
-			</svg>
+				
+				<svg viewBox="0 0 144 144" width="100%" height="100%" style="position: absolute; top: 0; left: 0;">
+					{#each $paths as path}
+						<polyline
+							class="curve"
+							points="{path.bezierCurvePoints.map(point => `${point.x},${144 - (point.y)}`).join(' ')}"
+							style="stroke: {path.color};"
+						/>
+					{/each}
+				</svg>
 
 
 
-			{#if $paths.length > 0}
-				<img src="/drivetrain.png" alt="Robot" id="robot" style="width: {robotWidth / 144 * 100}%; height: {robotLength / 144 * 100}%; left: {(robotX / 144) * 100}%; bottom: {(robotY / 144) * 100}%;" />
-			{/if}
+				{#if $paths.length > 0}
+					<img src="/drivetrain.png" alt="Robot" id="robot" style="width: {robotWidth / 144 * 100}%; height: {robotLength / 144 * 100}%; left: {(robotX / 144) * 100}%; bottom: {(robotY / 144) * 100}%; user-select: none;" />
+				{/if}
 
 
 
+			</div>
+			
+			<div class="paths">
+				{#each $paths as path}
+					<div class="path" style="border-color: {path.color};">
+						<div class="path-header">
+							<div class="path-and-color">
+								<svg class="drag-handle" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black"><path d="M160-360v-80h640v80H160Zm0-160v-80h640v80H160Z"/></svg>
+								<input type="color" class="color-circle" style="background-color: {path.color};" bind:value={path.color} on:input={() => updatePathColor(path.id, path.color)} />
+								<p class="path-title">Path {path.id + 1}</p>
+							</div>
+							<div class="add-and-remove">
+								<!-- svelte-ignore a11y-click-events-have-key-events -->
+								<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FF474D" on:click={() => deletePath(path.id)} style="cursor: pointer;"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+								<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#90EE90" on:click={() => addControlPointToPath(path.id)} on:keydown={(e) => { if (e.key === 'Enter') addControlPointToPath(path.id, x, y); }} style="cursor: pointer;"><path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+							</div>
+							
+						</div>
+							<div class="path-control-points">
+								{#each path.controlPoints as { x, y }, i}
+									<div class="control-point-box">
+										<label for="control-point-{path.id}-{i}">Control Point {i + 1}:</label>
+										<input id="control-point-{path.id}-{i}" class="standard-input-box" type="number" step="0.01" bind:value={path.controlPoints[i].x} on:input={() => generateBezierCurve(path.id)} />
+										<input id="control-point-{path.id}-{i}-y" class="standard-input-box" type="number" step="0.01" bind:value={path.controlPoints[i].y} on:input={() => generateBezierCurve(path.id)} />
+									</div>
+								{/each}
+						</div>
+					</div>
+				{/each}
+
+				<button on:click={() => {addPath(); generateBezierCurve($paths.length - 1);}}>Add Path</button>
+			</div>
 		</div>
-		
-		<div class="paths">
-			{#each $paths as path}
-				<div class="path" style="border-color: {path.color};">
-					<div class="path-header">
-						<div class="path-and-color">
-							<svg class="drag-handle" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black"><path d="M160-360v-80h640v80H160Zm0-160v-80h640v80H160Z"/></svg>
-							<input type="color" class="color-circle" style="background-color: {path.color};" bind:value={path.color} on:input={() => updatePathColor(path.id, path.color)} />
-							<p class="path-title">Path {path.id + 1}</p>
-						</div>
-						<div class="add-and-remove">
-							<!-- svelte-ignore a11y-click-events-have-key-events -->
-							<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FF474D" on:click={() => deletePath(path.id)} style="cursor: pointer;"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
-							<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#90EE90" on:click={() => addControlPointToPath(path.id)} on:keydown={(e) => { if (e.key === 'Enter') addControlPointToPath(path.id, x, y); }} style="cursor: pointer;"><path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
-						</div>
-						
-					</div>
-						<div class="path-control-points">
-							{#each path.controlPoints as { x, y }, i}
-								<div class="control-point-box">
-									<label for="control-point-{path.id}-{i}">Control Point {i + 1}:</label>
-									<input id="control-point-{path.id}-{i}" class="standard-input-box" type="number" step="0.01" bind:value={path.controlPoints[i].x} on:input={() => generateBezierCurve(path.id)} />
-									<input id="control-point-{path.id}-{i}-y" class="standard-input-box" type="number" step="0.01" bind:value={path.controlPoints[i].y} on:input={() => generateBezierCurve(path.id)} />
-								</div>
-							{/each}
-					</div>
-				</div>
-			{/each}
 
-			<button on:click={() => {addPath(); generateBezierCurve($paths.length - 1);}}>Add Path</button>
+		<div class="footer">
+			<div class="scrubbing-bar">
+				<div class="play-button">
+					<!-- svelte-ignore a11y-click-events-have-key-events -->
+					{#if !isPlaying}
+					 	<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#90EE90" on:click={playPath}><path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/></svg>
+					{:else}
+						<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FF474D" on:click={pausePath}><path d="M520-200v-560h240v560H520Zm-320 0v-560h240v560H200Zm400-80h80v-400h-80v400Zm-320 0h80v-400h-80v400Zm0-400v400-400Zm320 0v400-400Z"/></svg>
+					{/if}
+					
+				</div>
+				<input type="range" id="scrub" min="0" max="100" step="0.001" bind:value={scrubValue} on:input={updateRobotPosition} />
+			</div>
 		</div>
 
 		
