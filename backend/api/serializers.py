d25c47074c41770dce9326fc639caf9f9dbe76b2
JWhof <romanczyy@gmail.com>
2025-01-18 22:59:53 +0100

    stuff

--- backend/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Path
-
-class PathSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Path
-        fields = '__all__'
--- backend/api/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import Path
-from .serializers import PathSerializer
-
-class PathViewSet(viewsets.ModelViewSet):
-    queryset = Path.objects.all()
-    serializer_class = PathSerializer
